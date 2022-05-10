a4c9880ab5a2b1979f0a862862e36b4e31c61020
jerboa88 <jerboa88@users.noreply.github.com>
2022-05-10 20:42:49 +0000

    deploy: f781169b6fca708723f598e7778584b26db4927f

--- LICENSE-THIRD-PARTY.md
+++
@@ -1,5 +0,0 @@
-# Third-party Licenses
-
-## ["Pancakes" icon](https://thenounproject.com/icon/pancakes-1974111/) by Kokota at NounProject.com is licensed under [CC BY](https://creativecommons.org/licenses/by/3.0/) (with additional optimization and recoloring by me).
-
-## Other assorted icons are from [FontAwesome](http://fontawesome.io/) by Dave Gandy.
--- src/common/custom.d.ts
+++
@@ -1,9 +0,0 @@
-// Create custom type for svg files to suppress import warnings
-declare module '../images/*.svg' {
-	export default React.FunctionComponent<React.SVGAttributes<SVGElement>>();
-}
-
-// Create custom type for png images to suppress import warnings
-declare module '../images/*.png' {
-	export default React.FunctionComponent<React.PNGAttributes<HTMLImageElement>>();
-}
