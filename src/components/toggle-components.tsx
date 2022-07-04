5064b9fc4ea37f1538f4d44f8ada39f70016da52
John Goodliff <johny112009@hotmail.com>
2022-07-04 17:26:38 -0600

    Update styles for toggle component

--- src/components/toggle-components.tsx
+++ src/components/toggle-components.tsx
@@ -27,16 +27,20 @@ function Toggle(props: TogglePropsInterface) {
 	const { isEnabled, toggle } = useContext(props.context);
 
 	return (
-		<label className="p-4">
-			{/* Modify the html data attribute */}
-			<Helmet htmlAttributes={{ [props.htmlAttribute]: isEnabled ? props.enabled[0] : props.disabled[0] }} />
+		<div className="tooltip tooltip-primary" data-tip={`Toggle ${props.label}`}>
+			<div className="form-control">
+				<label className="label p-4 cursor-pointer">
+					{/* Modify the html data attribute */}
+					<Helmet htmlAttributes={{ [props.htmlAttribute]: isEnabled ? props.enabled[0] : props.disabled[0] }} />
 
-			<span className="sr-only">{props.label}</span>
-			<Icon icon={isEnabled ? props.enabled[1] : props.disabled[1]} tw="fa-lg mr-2 !align-middle" />
-			{/* For whatever reason the onChange event is not fired on the first toggle of the input element, so we have to use an onInput listener instead */}
-			{/* The readOnly attribute is specified on the input element to suppress warnings about the onChange event listener not being specified */}
-			<input type="checkbox" className="toggle toggle-primary align-middle has-motion" checked={isEnabled} onInput={toggle} readOnly />
-		</label>
+					<Icon icon={isEnabled ? props.enabled[1] : props.disabled[1]} tw="mr-2 !align-middle" />
+					<span className="label-text mr-4 text-base font-button font-bold uppercase">{props.label}</span>
+					{/* For whatever reason the onChange event is not fired on the first toggle of the input element, so we have to use an onInput listener instead */}
+					{/* The readOnly attribute is specified on the input element to suppress warnings about the onChange event listener not being specified */}
+					<input type="checkbox" className="toggle toggle-primary align-middle has-motion" checked={isEnabled} onInput={toggle} readOnly />
+				</label>
+			</div>
+		</div>
 	);
 }
 
@@ -46,13 +50,13 @@ function Toggle(props: TogglePropsInterface) {
 // A toggle component used to change the page theme
 export function ThemeToggle() {
 	return (
-		<Toggle label="Toggle theme" context={DarkThemeContext} htmlAttribute="data-theme" disabled={['light', faSun]} enabled={['dark', faMoon]} />
+		<Toggle label="dark theme" context={DarkThemeContext} htmlAttribute="data-theme" disabled={['light', faSun]} enabled={['dark', faMoon]} />
 	);
 }
 
 // A toggle component used to change the `motion-allowed` property
 export function MotionToggle() {
 	return (
-		<Toggle label="Toggle motion" context={AllowMotionContext} htmlAttribute="data-motion" disabled={['reduce', faWandMagic]} enabled={['allow', faWandMagicSparkles]} />
+		<Toggle label="enabled animations" context={AllowMotionContext} htmlAttribute="data-motion" disabled={['reduce', faWandMagic]} enabled={['allow', faWandMagicSparkles]} />
 	);
 }
